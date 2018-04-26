729e28c7c5e2768a66a14a736fcad58e5d77e471
Tim Docker <tim@dockerz.net>
2018-04-27 09:36:19 +1000

    Added a help subcommand with some hopefully useful text

--- README.md
+++ README.md
@@ -5,13 +5,80 @@ and release zip files from S3, unpacks into versioned directories,
 and runs commands to stop and start them as required.
 
 ```
+$ hx-deploy-tool
 Usage:
-  hx-deploy-tool fetch-context
-  hx-deploy-tool unpack <releaseid> <todir>
-  hx-deploy-tool select <releaseid>
+  hx-deploy-tool help
+  hx-deploy-tool fetch-context [--retry]
+  hx-deploy-tool list-releases
+  hx-deploy-tool unpack <release> <todir>
+  hx-deploy-tool select <release>
+  hx-deploy-tool aws-docker-login-cmd
 
 The config file is read from the file specified with HX_DEPLOY_CONFIG.
 It defaults to ../etc/hx-deploy-tool.json (relative to the executable).
+
+$ hx-deploy-tool help
+hx-deploy-tool is a simple tool to manage docker based application
+deployments at helix.
+
+A software release is a (typically small) zip archive containing
+pure configuration, typically a docker-compose file and other
+configuration files templates. `hx-deploy-tool` can fetch such
+releases from AWS S3, unpack them, configure it with appropriate
+environmental details and start the system.
+
+The hx-deploy-tool itself has a json formatted configuration file
+with a schema specfied in ADL (see config.adl in the source
+distribution). This specifies:
+
+* the local directories where releases are unpacked,
+* the AWS S3 location where release archives are installed.
+* the environment information available to configure packages
+
+See the source repository (https://github.com/helix-collective/hx-deploy-tool)
+for details.
+
+The following subcommands are available:
+
+# hx-deploy-tool help
+
+Shows this help text.
+
+# hx-deploy-tool fetch context [--retry]
+
+Downloads the environmental information files from AWS S3. The
+`--retry` option is useful during system bootstrap, and it is necessary
+to wait for the information files to be created.
+
+# hx-deploy-tool list-releases
+
+Shows the release archives available in S3, most recent first.
+
+# hx-deploy-tool unpack <release> <todir>
+
+Unpack and configure the specified  release into the given directory.
+
+# hx-deploy-tool aws-docker-login-cmd
+
+Assuming this is run on a AWS EC2 instance, this subcommand runs
+the appropriate docker login command using the instance profile
+to permit access to configured ECR repositories. This command is
+typically run on first boot.
+
+# hx-deploy-tool select <release>
+
+The command combines all of the necessary functions to replace
+any existing running release with the specified release.
+Specifically it:
+
+ - Fetches the release archive from S3
+ - Unpacks it
+ - configures it for the local environment
+ - runs the prestart script. This will typically pull
+   necessary docker images.
+ - stops the current release (if any)
+ - starts the new release
+ - switches the `current` symlink to point to the new release
 ```
 
 The tool is configured with an ADL specified json
--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@
 module Main where
 
 import qualified Data.Text as T
+import qualified Data.Text.IO as T
 
 import ADL.Config(ToolConfig(..))
 import ADL.Core(adlFromJsonFile')
@@ -11,17 +12,92 @@ import System.Exit(exitWith,ExitCode(..))
 import System.FilePath(takeDirectory, (</>))
 import System.Posix.Files(fileExist)
 
+usageText :: T.Text
+usageText = "\
+  \Usage:\n\
+  \  hx-deploy-tool help\n\
+  \  hx-deploy-tool fetch-context [--retry]\n\
+  \  hx-deploy-tool list-releases\n\
+  \  hx-deploy-tool unpack <release> <todir>\n\
+  \  hx-deploy-tool select <release>\n\
+  \  hx-deploy-tool aws-docker-login-cmd\n\
+  \\n\
+  \The config file is read from the file specified with HX_DEPLOY_CONFIG.\n\
+  \It defaults to ../etc/hx-deploy-tool.json (relative to the executable).\n\
+  \"
+
+helpText :: T.Text
+helpText = "\
+  \hx-deploy-tool is a simple tool to manage docker based application\n\
+  \deployments at helix.\n\
+  \\n\
+  \A software release is a (typically small) zip archive containing\n\
+  \pure configuration, typically a docker-compose file and other\n\
+  \configuration files templates. `hx-deploy-tool` can fetch such\n\
+  \releases from AWS S3, unpack them, configure it with appropriate\n\
+  \environmental details and start the system.\n\
+  \ \n\
+  \The hx-deploy-tool itself has a json formatted configuration file\n\
+  \with a schema specfied in ADL (see config.adl in the source\n\
+  \distribution). This specifies:\n\
+  \\n\
+  \* the local directories where releases are unpacked,\n\
+  \* the AWS S3 location where release archives are installed.\n\
+  \* the environment information available to configure packages\n\
+  \\n\
+  \See the source repository (https://github.com/helix-collective/hx-deploy-tool)\n\
+  \for details.\n\
+  \\n\
+  \The following subcommands are available:\n\
+  \\n\
+  \# hx-deploy-tool help\n\
+  \\n\
+  \Shows this help text.\n\
+  \\n\
+  \# hx-deploy-tool fetch context [--retry]\n\
+  \\n\
+  \Downloads the environmental information files from AWS S3. The\n\
+  \`--retry` option is useful during system bootstrap, and it is necessary\n\
+  \to wait for the information files to be created.\n\
+  \\n\
+  \# hx-deploy-tool list-releases\n\
+  \\n\
+  \Shows the release archives available in S3, most recent first.\n\
+  \\n\
+  \# hx-deploy-tool unpack <release> <todir>\n\
+  \\n\
+  \Unpack and configure the specified  release into the given directory.\n\
+  \\n\
+  \# hx-deploy-tool aws-docker-login-cmd\n\
+  \\n\
+  \Assuming this is run on a AWS EC2 instance, this subcommand runs\n\
+  \the appropriate docker login command using the instance profile\n\
+  \to permit access to configured ECR repositories. This command is\n\
+  \typically run on first boot.\n\
+  \\n\
+  \# hx-deploy-tool select <release>\n\
+  \\n\
+  \The command combines all of the necessary functions to replace\n\
+  \any existing running release with the specified release.\n\
+  \Specifically it:\n\
+  \\n\
+  \ - Fetches the release archive from S3\n\
+  \ - Unpacks it\n\
+  \ - configures it for the local environment\n\
+  \ - runs the prestart script. This will typically pull\n\
+  \   necessary docker images.\n\
+  \ - stops the current release (if any)\n\
+  \ - starts the new release\n\
+  \ - switches the `current` symlink to point to the new release\n\
+  \"
+
 usage :: IO ()
 usage = do
-  putStrLn "Usage:"
-  putStrLn "  hx-deploy-tool fetch-context [--retry]"
-  putStrLn "  hx-deploy-tool list-releases"
-  putStrLn "  hx-deploy-tool unpack <releaseid> <todir>"
-  putStrLn "  hx-deploy-tool select <releaseid>"
-  putStrLn "  hx-deploy-tool aws-docker-login-cmd"
-  putStrLn ""
-  putStrLn "The config file is read from the file specified with HX_DEPLOY_CONFIG."
-  putStrLn "It defaults to ../etc/hx-deploy-tool.json (relative to the executable)."
+  T.putStrLn usageText
+
+help :: IO ()
+help = do
+  T.putStrLn helpText
 
 getConfigPath :: IO FilePath
 getConfigPath = do
@@ -37,6 +113,8 @@ main = do
   args <- getArgs
   configPath <- getConfigPath
   case args of
+    ["help"] -> do
+      help
     ["fetch-context"] -> do
       config <- adlFromJsonFile' configPath
       fetchContext config Nothing
