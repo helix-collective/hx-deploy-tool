fed39a7c1cef08296bf8a8d09f07972ec7fba98c
Tim Docker <tim@dockerz.net>
2018-12-27 21:10:11 +1100

    Use dockerized adlc, remove generated typescript
    We don't need the typescript generated from the ADL, as we now
    generated it where we need it.

--- scripts/gen-adl.sh
+++ scripts/gen-adl.sh
@@ -2,26 +2,8 @@
 set -ex
 
 SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-REPO_ROOT="$SCRIPT_DIR"/..
-
-if [ -z "${ADLC-}" ]; then
-    # Setup ADL from stack:
-    if [ "${NIX_PATH-}" ]; then
-        STACK="stack --nix"
-    else
-        STACK="stack"
-    fi
-
-    (cd $REPO_ROOT; $STACK setup 2>/dev/null && $STACK build adl-compiler)
-    export ADLC=$(cd $REPO_ROOT; $STACK path --local-install-root)/bin/adlc
-fi
-
-ROOT="${REPO_ROOT}"
+ROOT=$SCRIPT_DIR/..
+ADLC=$SCRIPT_DIR/adlc
 ADLSTDLIB="$(${ADLC} show --adlstdlib)"
 rm -rf $ROOT/src/ADL
-${ADLC} haskell -O $REPO_ROOT/src --package=ADL --rtpackage=ADL.Core --include-rt -I $ROOT/adl $ROOT/adl/types.adl $ROOT/adl/config.adl $ROOT/adl/release.adl $ROOT/adl/state.adl $ADLSTDLIB/sys/types.adl
-
-rm -rf $REPO_ROOT/typescript
-${ADLC} typescript -O $REPO_ROOT/typescript/ --include-rt --runtime-dir runtime -I $ROOT/adl $ROOT/adl/types.adl $ROOT/adl/config.adl $ADLSTDLIB/sys/types.adl
-# Add an empty tslint config to prevent lint errors from generated code.
-echo '{}' > $REPO_ROOT/typescript/tslint.json
+${ADLC} haskell -O $ROOT/src --package=ADL --rtpackage=ADL.Core --include-rt -I $ROOT/adl $ROOT/adl/types.adl $ROOT/adl/config.adl $ROOT/adl/release.adl $ROOT/adl/state.adl $ADLSTDLIB/sys/types.adl
