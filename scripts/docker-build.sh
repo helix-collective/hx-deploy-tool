c65505341e3e36f76c5c6f3901c1df7bf13b2cfa
Tim Docker <tim@dockerz.net>
2019-05-22 09:45:05 +1000

    Use stack's builtin docker functionality to build deployable release

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -3,19 +3,19 @@
 # Helper script to build under docker, for linux deployment.
 # Write a compressed binary to /tmp/hx-deploy-tool.gz
 
+set -ex
+
 USER=$(whoami)
 USERID=$(id -u)
 GROUPID=$(id -g)
 
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
 
-docker run -it --rm \
- -v /home/$USER/.stack:/home/$USER/.stack \
- -v $ROOT:$ROOT \
- -v /etc/passwd:/etc/passwd \
- -v /etc/group:/etc/group \
- -v /tmp:/tmp \
- --user $USERID:$GROUPID \
- -w $ROOT \
- helixta/helixdev:2018-04-17 \
- /bin/bash -c "stack --no-nix build && cp `stack --no-nix exec which -- hx-deploy-tool` /tmp/hx-deploy-tool.$(arch)-linux && gzip -f /tmp/hx-deploy-tool.$(arch)-linux"
+
+stack docker pull
+stack --docker build
+EXE=$(stack --docker exec which hx-deploy-tool)
+echo EXE $EXE
+cp $EXE /tmp/hx-deploy-tool.$(arch)-linux 
+gzip -f /tmp/hx-deploy-tool.$(arch)-linux
+
